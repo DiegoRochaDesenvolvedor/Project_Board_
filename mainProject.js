ead4f3bac6a0ac9f4e33fa73b35029b3a9e28dae
DiegoRochaDesenvolvedor <102746033+DiegoRochaDesenvolvedor@users.noreply.github.com>
2023-03-12 18:07:03 -0300

    Update mainProject.js

--- mainProject.js
+++ mainProject.js
@@ -33,7 +33,6 @@ document.addEventListener('keydown',(e)=>{
         inputInsertKey(input);
     }
 });
-
 functions.readTable(table()[0].tableRead);//read list todo
 functions.loopButton(table()[0].tableRead);//add ok buttons function action
 functions.loopButtonBack(table()[0].tableRead);//add back buttons function action
