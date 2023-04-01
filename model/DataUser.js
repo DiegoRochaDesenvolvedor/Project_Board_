8f9ca1faa20bd22fd3e5884b6fc6434169cadd60
DiegoRochaDesenvolvedor <102746033+DiegoRochaDesenvolvedor@users.noreply.github.com>
2023-04-01 19:50:28 -0300

    Add dsh

--- model/DataUser.js
+++ model/DataUser.js
@@ -15,6 +15,7 @@ module.exports = class DataUser{
                 "profile": this.profile,
                 "group": this.group
         }]
+        
         return form 
     }
     createUser(){
